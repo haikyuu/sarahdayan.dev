ba58c5f7d6bea7fca4540cc2da53ec2e215bede1
Sarah Dayan <dayan.sarah@gmail.com>
2020-01-10 22:32:52 +0100

    chore: add meta description

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ import DefaultLayout from "~/layouts/Default.vue";
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
+
+  head.meta.push({
+    name: "description",
+    content: "Sarah Dayan | Senior Software Engineer at Algolia | Creator of Dinero.js | TypeScript, JavaScript, Vue.js, CSS"
+  });
 }
