1841c4564f73bde64bd9f95788b7ce166e401c61
Sarah Dayan <dayan.sarah@gmail.com>
2020-01-11 20:40:45 +0100

    chore: add link styles

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -9,19 +9,19 @@
           <p class="mt-4">
             I’m a Senior Software Engineer currently working as Tech Lead of the
             Doc Squad at
-            <a href="https://www.algolia.com/" target="_blank" rel="noopener"
+            <a class="underline" href="https://www.algolia.com/" target="_blank" rel="noopener"
               >Algolia</a
             >. I mostly do front-end development, and I’m a
-            <a href="https://vuejs.org/" rel="noopener">Vue.js</a> and CSS nerd.
+            <a class="underline" href="https://vuejs.org/" rel="noopener">Vue.js</a> and CSS nerd.
             I can't shut up about test-driven development and utility-first CSS.
             I also share what I learn on my blog
-            <a href="http://frontstuff.io" target="_blank" rel="noopener"
+            <a class="underline" href="http://frontstuff.io" target="_blank" rel="noopener"
               >frontstuff.io</a
             >, or at <g-link to="/talks"> meetups and conferences</g-link>.
           </p>
           <p class="mt-4">
             Among other things, I created
-            <a href="https://github.com/sarahdayan/dinero.js" rel="noopener"
+            <a class="underline" href="https://github.com/sarahdayan/dinero.js" rel="noopener"
               >Dinero.js</a
             >, a multi-environment JavaScript library to handle monetary values.
           </p>
